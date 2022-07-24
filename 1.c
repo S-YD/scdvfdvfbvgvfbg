30a04e105230c0a22690bec178384819cab8f4a7
S-YD <3157364817@qq.com>
2022-07-24 10:04:40 +0800

    SYD2022724

--- 1.c
+++ 1.c
@@ -1,1 +1,11 @@
-zcscbdbhchbdhbmshbm
+#include <stdio.h>
+#include <string.h>
+int main(int argc, char **argv)
+{
+    printf ("xhsxhh\n");
+    char *p = "GHJAHBASSHJJHA";
+    char *q = "GHJAHBASSHJJHA";
+    
+    printf("%d\n", strcmp(q, p));
+    return 0;
+}

74cadc105e0f88f28180966571b3fd921434113c
S-YD <3157364817@qq.com>
2022-07-24 10:33:28 +0800

    SYD2022724

--- 1.c
+++ 1.c
@@ -5,7 +5,7 @@ int main(int argc, char **argv)
     printf ("xhsxhh\n");
     char *p = "GHJAHBASSHJJHA";
     char *q = "GHJAHBASSHJJHA";
-    
+    printf ("%s\n", p);
     printf("%d\n", strcmp(q, p));
     return 0;
 }
